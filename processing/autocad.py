f42005b2c9fb2802fccd844c1a9021941b2dd96c
maxi <by.mikky1206@gmail.com>
2024-11-06 00:44:43 +0300

    исправление кода корректировки данных, полученных из чертежа таксации

--- processing/autocad.py
+++ processing/autocad.py
@@ -1,8 +1,11 @@
+from itertools import chain
 from typing import Union
 import ezdxf
 from ezdxf.entities import Text, MText, Line, LWPolyline
 from shapely.geometry import Point, LineString, Polygon
 from pathlib import Path
+
+from shapely.ops import unary_union
 
 
 class TaxationObject:
@@ -12,6 +15,23 @@ class TaxationObject:
 
     def __repr__(self) -> str:
         return f"TaxationObject({self.number}, {self.shape})"
+
+
+class TaxationCollection:
+    trees: list[TaxationObject]
+    lines: list[TaxationObject]
+    contours: list[TaxationObject]
+    zones: list[TaxationObject]
+
+    def __init__(self) -> None:
+        self.trees = list()
+        self.lines = list()
+        self.contours = list()
+        self.zones = list()
+
+    def __repr__(self) -> str:
+        return (f"TaxationCollection(Trees:{len(self.trees)}, Lines:{len(self.lines)}, Contours:{len(self.contours)}, "
+                f"Zones:{len(self.zones)})")
 
 
 def extract_data_from_taxation_plan(file_path: Path,
@@ -50,6 +70,10 @@ def extract_data_from_taxation_plan(file_path: Path,
 
         elif isinstance(entity, LWPolyline) and entity.dxf.layer in zones_layers:
             zones.append(entity)
+        elif isinstance(entity, MText) and entity.dxf.layer in zones_layers:
+            zones.append(entity)
+        elif isinstance(entity, Text) and entity.dxf.layer in zones_layers:
+            zones.append(entity)
 
     return numbers, lines, contours, zones
 
@@ -57,32 +81,66 @@ def extract_data_from_taxation_plan(file_path: Path,
 def correcting_data_from_taxation_plan(numbers: list, lines: list, contours: list, zones: list):
     """Метод для коррекции номеров"""
 
-    data = {
-        "numbers": [],
-        "lines": [],
-        "contours": [],
-        "zones": []
-    }
+    taxation_collection_temp = TaxationCollection()
+    taxation_collection = TaxationCollection()
+
+    zone_names = []
+    zones_temp = []
+    for zone in zones:
+        if isinstance(zone, LWPolyline):
+            zones_temp.append(Polygon([(float(x), float(y)) for x, y in list(zone.vertices())]))
+        if isinstance(zone, Text) or isinstance(zone, MText):
+            number = zone.plain_text()
+            x, y = zone.dxf.insert[0], zone.dxf.insert[1]
+            zone_names.append(TaxationObject(number, Point(x, y)))
+    zone_chunks = {}
+    for zone_name in zone_names:
+        if zone_name.number not in zone_chunks.keys():
+            zone_chunks[zone_name.number] = []
+        for polygon in zones_temp:
+            if zone_name.shape.distance(polygon.exterior) < 0.01:
+                zone_chunks[zone_name.number].append(polygon)
+    for name, polygons in zone_chunks.items():
+        taxation_collection.zones.append(TaxationObject(name, unary_union(polygons)))
 
     for text in numbers:
         number = text.plain_text().replace('\n', ' ') if isinstance(text, MText) else text.plain_text()
-        x, y = text.dxf.insert[1], text.dxf.insert[0]
-        data["numbers"].append(TaxationObject(number, Point(x, y)))
+        x, y = text.dxf.insert[0], text.dxf.insert[1]
+        taxation_collection_temp.trees.append(TaxationObject(number, Point(x, y)))
 
     for line in lines:
         if isinstance(line, LWPolyline):
             coordinates = [(float(x), float(y)) for x, y in list(line.vertices())]
-            data["lines"].append(TaxationObject(None, LineString(coordinates)))
+            taxation_collection_temp.lines.append(TaxationObject(None, LineString(coordinates)))
         if isinstance(line, Line):
             coordinates = [(line.dxf.start.x, line.dxf.start.y), (line.dxf.end.x, line.dxf.end.y)]
-            data["lines"].append(TaxationObject(None, LineString(coordinates)))
+            taxation_collection_temp.lines.append(TaxationObject(None, LineString(coordinates)))
 
     for contour in contours:
-        data["contours"].append(
+        taxation_collection_temp.contours.append(
             TaxationObject(None, Polygon([(float(x), float(y)) for x, y in list(contour.vertices())]))
         )
 
-    for zone in zones:
-        data["zones"].append(TaxationObject(None, Polygon([(float(x), float(y)) for x, y in list(zone.vertices())])))
+    for number in taxation_collection_temp.trees:
+        for line in taxation_collection_temp.lines:
+            if line.shape.distance(number.shape) < 0.01:
+                line_ = TaxationObject(number.number, line.shape)
+                taxation_collection.lines.append(line_)
+                break
+        for polygon in taxation_collection_temp.contours:
+            if number.shape.distance(polygon.shape.exterior) < 0.01:
+                polygon_ = TaxationObject(number.number, polygon.shape)
+                taxation_collection.contours.append(polygon_)
+                break
 
-    return data
+    all_numbers = set([_.number for _ in taxation_collection_temp.trees])
+    shapes_numbers = set([_.number for _ in chain(taxation_collection.lines, taxation_collection.contours)])
+    numbers_of_trees = all_numbers - shapes_numbers
+
+    for number in taxation_collection_temp.trees:
+        if number.number in numbers_of_trees:
+            taxation_collection.trees.append(number)
+
+    # TODO: разделить номера типа 24-25 и 19а-в на 24,25 и 19а,19б,19в
+
+    return taxation_collection
