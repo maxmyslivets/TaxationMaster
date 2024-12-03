35860118700df6ecd6715fc4acf727e5b1c3c5d3
maxi <by.mikky1206@gmail.com>
2024-12-03 22:57:46 +0300

    обработан топографический план

--- src/processing/taxation_list.py
+++ src/processing/taxation_list.py
@@ -3,70 +3,28 @@ from pathlib import Path
 import pandas as pd
 from docx import Document as DocxDocument
 
-from src.objects import TaxationList
 from src.processing.splitting_numbers import split_number
 
 
-def _create_taxation_list(table_data: list[list[str]]) -> TaxationList:
+def _create_taxation_list(table_data: list[list[str]]) -> pd.DataFrame:
     """Создание ведомости таксации из табличных данных"""
 
-    taxation_list = TaxationList()
+    taxation_list = []
 
     # Создаем DataFrame из исходных данных
-    taxation_list.numbers_df = pd.DataFrame(
+    table_data_df = pd.DataFrame(
         table_data,
-        columns=['number', 'name', 'quantity', 'height', 'diameter', 'quality']
+        columns=['origin_number', 'name', 'quantity', 'height', 'diameter', 'quality']
     )
 
-    # Разделение номеров
-    splitted_numbers = []
-    for _, row in taxation_list.numbers_df.iterrows():
-        items = [item.strip() for item in row['number'].split(",")]
-        for number in items:
-            splitted_list = split_number(number)
-            if splitted_list:
-                for splitted_number in splitted_list:
-                    splitted_numbers.append({
-                        'split_number': splitted_number,
-                        'original_number': row['number'],
-                        'is_bug': False,
-                        'name': row['name'],
-                        'quantity': row['quantity'],
-                        'height': row['height'],
-                        'diameter': row['diameter'],
-                        'quality': row['quality']
-                    })
-            else:
-                splitted_numbers.append({
-                    'split_number': number,
-                    'original_number': row['number'],
-                    'is_bug': True,
-                    'name': row['name'],
-                    'quantity': row['quantity'],
-                    'height': row['height'],
-                    'diameter': row['diameter'],
-                    'quality': row['quality']
-                })
+    # TODO
 
-    # Создаем DataFrame для разделенных номеров
-    taxation_list.splitted_numbers_df = pd.DataFrame(splitted_numbers)
-
-    # Формируем итоговую таблицу
-    taxation_list.table_data = taxation_list.splitted_numbers_df[[
-        'splitted_number', 'original_number', 'name', 'quantity', 'height', 'diameter', 'quality'
-    ]].copy()
-
-    # Переименовываем колонку original_number в number
-    taxation_list.table_data = taxation_list.table_data.rename(
-        columns={'original_number': 'number'}
-    )
-
-    return taxation_list
+    return table_data_df
 
 
 def create_taxation_list(file_path: Path,
                          is_import_first_row: bool = False,
-                         column_mapping: dict = None) -> TaxationList:
+                         column_mapping: dict = None) -> pd.DataFrame:
     """Создание ведомости таксации из файла docx/xlsx"""
 
     if column_mapping is None:
--- src/processing/topographic_plan.py
+++ src/processing/topographic_plan.py
@@ -5,16 +5,33 @@ import pandas as pd
 from ezdxf.entities import MText
 from shapely import LineString, Polygon, Point
 
-from src.objects import TopographicPlan
 from src.processing.splitting_numbers import split_number
 
 
 def create_topographic_plan(dxf_path: Path, numbers_layers: list[str],
                             lines_layers: list[str], contours_layers: list[str],
-                            min_distance: float) -> TopographicPlan | None:
-    valid = True
+                            max_distance: float) -> pd.DataFrame:
+    """
+    Создание DataFrame объектов топографического плана.
+
+    Series
+    ----
+    * origin_number - оригинальный номер.
+    * number_position - координаты номера.
+    * split_number - разделенный номер.
+    * type - тип геометрии объекта.
+    * geometry - геометрия объекта.
+    * size - размер геометрии объекта (длина | площадь | None).
+
+    :param dxf_path: Путь до dxf файла.
+    :param numbers_layers: Список слоев с номерами.
+    :param lines_layers: Список слоев с полосами растительности.
+    :param contours_layers: Список слоев с контурами растительности.
+    :param max_distance: Максимальное расстояние между номером и фигурой для их соединения.
+    :return: DataFrame | None
+    """
+
     doc = ezdxf.readfile(dxf_path)
-    topographical_plan = TopographicPlan()
 
     # Сбор номеров и их позиций
     numbers_data = []
@@ -31,9 +48,7 @@ def create_topographic_plan(dxf_path: Path, numbers_layers: list[str],
                 'position': Point(text.dxf.insert[0], text.dxf.insert[1])
             })
 
-    topographical_plan._numbers_df = pd.DataFrame(numbers_data)
-    if not topographical_plan._numbers_df.empty:
-        topographical_plan._numbers_df.index.name = 'id'
+    numbers_df = pd.DataFrame(numbers_data)
 
     # Сбор геометрических фигур
     shapes_data = []
@@ -68,18 +83,16 @@ def create_topographic_plan(dxf_path: Path, numbers_layers: list[str],
                 'type': 'Polygon'
             })
 
-    topographical_plan._shapes_df = pd.DataFrame(shapes_data)
-    if not topographical_plan._shapes_df.empty:
-        topographical_plan._shapes_df.index.name = 'id'
+    shapes_df = pd.DataFrame(shapes_data)
 
     # Связывание номеров и фигур
     numbers_shapes_data = []
-    if not topographical_plan._numbers_df.empty and not topographical_plan._shapes_df.empty:
+    if not numbers_df.empty and not shapes_df.empty:
         # Создаем геометрические точки для всех номеров
-        number_points = topographical_plan._numbers_df.apply(lambda row: row['position'], axis=1)
+        number_points = numbers_df.apply(lambda row: row['position'], axis=1)
 
         # Для каждой фигуры проверяем все номера
-        for shape_id, shape_row in topographical_plan._shapes_df.iterrows():
+        for shape_id, shape_row in shapes_df.iterrows():
             shape_type = shape_row['type']
             shape_geom = shape_row['geometry']
 
@@ -90,7 +103,7 @@ def create_topographic_plan(dxf_path: Path, numbers_layers: list[str],
                 distances = number_points.apply(lambda p: p.distance(shape_geom.exterior))
 
             # Находим номера, которые находятся достаточно близко
-            close_numbers = distances[distances < min_distance]
+            close_numbers = distances[distances < max_distance]
 
             for number_id in close_numbers.index:
                 numbers_shapes_data.append({
@@ -98,117 +111,54 @@ def create_topographic_plan(dxf_path: Path, numbers_layers: list[str],
                     'shape_id': shape_id
                 })
 
-    topographical_plan._numbers_shapes_df = pd.DataFrame(numbers_shapes_data)
-
-    # # Валидация
-    # if not topographical_plan.numbers_shapes_df.empty:
-    #     duplicates = (topographical_plan.numbers_shapes_df.groupby('number_id')
-    #                   .size()
-    #                   .reset_index(name='count')
-    #                   .query('count > 1'))
-    #
-    #     if not duplicates.empty:
-    #         valid = False
-    #         for _, row in duplicates.iterrows():
-    #             number = topographical_plan.numbers_df.loc[row['number_id'], 'number']
-    #             print(f"[ERROR]\tНомер `{number}` встречается в чертеже на {row['count']} фигурах.")
+    numbers_shapes_df = pd.DataFrame(numbers_shapes_data)
 
     # Сбор деревьев (точечных объектов)
-    if not topographical_plan._numbers_df.empty and not topographical_plan._numbers_shapes_df.empty:
-        unassigned_numbers = topographical_plan._numbers_df.index.difference(
-            topographical_plan._numbers_shapes_df['number_id'])
+    unassigned_numbers = numbers_df.index.difference(numbers_shapes_df['number_id'])
 
-        trees_data = []
-        for number_id in unassigned_numbers:
-            trees_data.append({
-                'number_id': number_id
-            })
+    trees_data = []
+    for number_id in unassigned_numbers:
+        trees_data.append({
+            'number_id': number_id
+        })
 
-        topographical_plan._trees_df = pd.DataFrame(trees_data)
+    trees_df = pd.DataFrame(trees_data)
 
-    # # Разделение номеров
-    # splitted_numbers = []
-    # for _, row in topographical_plan.numbers_df.iterrows():
-    #     items = [item.strip() for item in row['number'].split(",")]
-    #     for number in items:
-    #         splitted_numbers_list = split_number(number)
-    #         if splitted_numbers_list:
-    #             for splitted_number in splitted_numbers_list:
-    #                 splitted_numbers.append({
-    #                     'splitted_number': splitted_number,
-    #                     'original_number': row['number'],
-    #                     'is_bug': False
-    #                 })
-    #         else:
-    #             splitted_numbers.append({
-    #                 'splitted_number': number,
-    #                 'original_number': row['number'],
-    #                 'is_bug': True
-    #             })
-    #
-    # topographical_plan.splitted_numbers_df = pd.DataFrame(splitted_numbers)
-    #
-    # if not valid:
-    #     print("[ERROR]\tЧертеж таксации содержит ошибки и не будет обработан.")
-    #     return None
-    #
-    # # Формирование итоговой таблицы
-    # table_data = []
-    # processed_numbers = set()  # Для отслеживания уже обработанных номеров
-    #
-    # for _, row in topographical_plan.splitted_numbers_df.iterrows():
-    #     splitted_number = row['splitted_number']
-    #
-    #     # Пропускаем, если номер уже обработан
-    #     if splitted_number in processed_numbers:
-    #         continue
-    #
-    #     base_data = {
-    #         'splitted_number': splitted_number,
-    #         'number': row['original_number']
-    #     }
-    #
-    #     # Находим связанные фигуры для данного номера
-    #     if not topographical_plan.numbers_shapes_df.empty:
-    #         number_shapes = topographical_plan.numbers_shapes_df.merge(
-    #             topographical_plan.shapes_df,
-    #             on='shape_id'
-    #         )
-    #         shape_data = number_shapes[
-    #             number_shapes['number_id'].astype(str) == str(row['original_number'])
-    #             ]
-    #
-    #         if not shape_data.empty:
-    #             # Берем первую найденную фигуру
-    #             shape = shape_data.iloc[0]
-    #             if shape['type'] == 'LineString':
-    #                 table_data.append({
-    #                     **base_data,
-    #                     'type_shape': 'Полоса',
-    #                     'value': str(round(shape['geometry'].length, 1)),
-    #                     'unit': 'м.п.'
-    #                 })
-    #             else:
-    #                 table_data.append({
-    #                     **base_data,
-    #                     'type_shape': 'Контур',
-    #                     'value': str(round(shape['geometry'].area, 1)),
-    #                     'unit': 'м2'
-    #                 })
-    #             processed_numbers.add(split_number)
-    #             continue
-    #
-    #     # Если нет связанных фигур, значит это дерево
-    #     if split_number not in processed_numbers:
-    #         table_data.append({
-    #             **base_data,
-    #             'type_shape': 'Дерево',
-    #             'value': '-',
-    #             'unit': 'шт.'
-    #         })
-    #         processed_numbers.add(split_number)
-    #
-    # # Создаем DataFrame без использования drop_duplicates
-    # topographical_plan.table_data = pd.DataFrame(table_data)
+    # Сбор итоговой таблицы с разделенными номерами
+    topographic_plan_data = []
 
-    return topographical_plan
+    for number_id in trees_df['number_id']:
+        for _split_number in split_number(numbers_df.iloc[number_id]['number']):
+            topographic_plan_data.append(
+                {
+                    'origin_number': numbers_df.iloc[number_id]['number'],
+                    'number_position': numbers_df.iloc[number_id]['position'],
+                    'split_number': _split_number,
+                    'type': 'Point',
+                    'geometry': numbers_df.iloc[number_id]['position'],
+                    'size': None
+                }
+            )
+
+    for numbers_shapes_id in numbers_shapes_df.index:
+
+        number_id = numbers_shapes_df.iloc[numbers_shapes_id]['number_id']
+        shape_id = numbers_shapes_df.iloc[numbers_shapes_id]['shape_id']
+        shape: LineString | Polygon = shapes_df.iloc[shape_id]['geometry']
+        shape_type = shapes_df.iloc[shape_id]['type']
+
+        for _split_number in split_number(numbers_df.iloc[number_id]['number']):
+            topographic_plan_data.append(
+                {
+                    'origin_number': numbers_df.iloc[number_id]['number'],
+                    'number_position': numbers_df.iloc[number_id]['position'],
+                    'split_number': _split_number,
+                    'type': shape_type,
+                    'geometry': shape,
+                    'size': shape.length if isinstance(shape, LineString) else shape.area
+                }
+            )
+
+    topographic_plan = pd.DataFrame(topographic_plan_data)
+
+    return topographic_plan
