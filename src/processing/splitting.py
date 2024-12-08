1f153693f74f4867d1accb7b347584a4461cadd9
maxi <by.mikky1206@gmail.com>
2024-12-08 16:19:26 +0300

    исправлены регулярные выражения и добавлено новое. Добавлено ручное разделение номеров и удаление дубликатов

--- src/processing/splitting.py
+++ src/processing/splitting.py
@@ -8,22 +8,39 @@ def split_number(number: str) -> list[str | None]:
     :return: Список разделенных номеров
     """
 
-    if re.match(r'^\d+$', number):
-        return [number]
+    numbers = []
 
-    elif re.match(r'^\d+[а-я]$', number):
-        return [number]
+    for part in number.split(','):
+        part = part.strip()
 
-    elif re.match(r'^\d+-\d+$', number):
-        start, end = map(int, number.split('-'))
-        return [str(i) for i in range(start, end + 1)]
+        if re.match(r'^\d+$', part):
+            numbers.append(part)
 
-    elif re.match(r'^\d+[а-я]-[а-я]$', number):
-        num = re.match(r'\d+', number).group()
-        start_letter, end_letter = re.findall(r'[а-я]', number)
-        return [f"{num}{chr(i)}" for i in range(ord(start_letter), ord(end_letter) + 1)]
+        elif re.match(r'^\d+\.\d+$', part):
+            numbers.append(part)
 
-    return [None]
+        elif re.match(r'^\d+[а-я]$', part):
+            numbers.append(part)
+
+        elif re.match(r'^\d+-\d+$', part):
+            start, end = map(int, part.split('-'))
+            numbers.extend([str(i) for i in range(start, end + 1)])
+
+        elif re.match(r'^\d+\.\d+-\d+\.\d+$', part):
+            start, end = part.split('-')
+            start_main, start_sub = map(int, start.split('.'))
+            end_main, end_sub = map(int, end.split('.'))
+            if start_main != end_main:
+                numbers.append(None)
+            else:
+                numbers.extend([f"{start_main}.{i}" for i in range(start_sub, end_sub + 1)])
+
+        elif re.match(r'^\d+[а-я]-[а-я]$', part):
+            num = re.match(r'\d+', part).group()
+            start_letter, end_letter = re.findall(r'[а-я]', part)
+            numbers.extend([f"{num}{chr(i)}" for i in range(ord(start_letter), ord(end_letter) + 1)])
+
+    return numbers if len(numbers) > 0 else [None]
 
 
 def split_tree_size(tree_size: str) -> list[str]:
